34005a3a311bc9ece4c50e8e8c94a4a473ab670a
sarmadsa <sarmad@virusoftware.com>
2018-06-17 21:58:14 +0200

    Create fps hexagon

---
+++ src/components/Hexagon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/Hexagon.css';
+
+const Hexagon = () =>{
+    return(
+        <div className="hexagon">
+            <div className="hexagon-score">
+                <p>100+</p>
+                <p>FPS</p>
+            </div>
+        </div>
+    );
+};
+
+export default Hexagon;
