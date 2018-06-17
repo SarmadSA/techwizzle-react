c085fcd02f5e6683511286ef736723bc355250c6
sarmadsa <sarmad@virusoftware.com>
2018-06-17 14:26:07 +0200

    Delete logo image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      {/* creat a heder component and add it here */}
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
