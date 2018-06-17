484f9d990a377c4b17012a076a76e8d736a2bc2b
sarmadsa <sarmad@virusoftware.com>
2018-06-17 21:57:01 +0200

    Create card component

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import CardHeader from './CardHeader';
+import GameShowcase from './GameShowcase';
+import '../css/Card.css';
+
+const Card = () =>{
+    return(
+        <div className="card">
+            <CardHeader />
+            <h4 className="card-content-tittle">Preformance in games:</h4>
+            <GameShowcase />
+            <GameShowcase />
+            <GameShowcase />
+            <div class="more-gameresults">
+                <p>More</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
