078992fd382d64df22b905d7341e4bfcb60649c3
sarmadsa <sarmad@virusoftware.com>
2018-06-18 19:33:54 +0200

    Divide main to sections

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Card from './Card';
+import SectionTitle from './SectionTitle'
 import '../css/Main.css';
 
 const Main = () =>{
     return (
         <main className="main">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+
+            <section>
+                <SectionTitle> Featured Product Profiles </SectionTitle>
+                <Card />
+                <Card />
+                <Card />
+            </section>
+
+            <section>
+                <SectionTitle> New Product Profiles </SectionTitle>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </section>
+
             <br className="clear"/>
         </main>
     );
