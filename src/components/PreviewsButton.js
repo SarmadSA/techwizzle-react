67103eb1df4458ca198005521b1e15d4edbab616
sarmadsa <sarmad@virusoftware.com>
2018-06-19 22:23:58 +0200

    Add slider prev button

---
+++ src/components/PreviewsButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../css/NavigationButton.css';
+
+const PreviewsButton = () =>{
+    return(
+        <div className="previews-slide">						
+            <button type="button" className="slide-button" onclick="prevSlide()">
+                <svg className="button-icon">
+                    <defs>
+                        <symbol id="button-icon-left" viewBox="0 0 32 32">
+                            <path d="M20.914 9.914l-2.829-2.829-8.914 8.914 8.914 8.914 2.828-2.828-6.086-6.086z"></path>
+                        </symbol>
+                    </defs>
+                    <use xlinkHref="#button-icon-left"></use>
+                </svg>
+            </button>
+        </div>
+    );
+};
+
+export default PreviewsButton;
