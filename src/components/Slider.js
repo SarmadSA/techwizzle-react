b8cb11b0096e872c172f7f962c35cb11a5427170
sarmadsa <sarmad@virusoftware.com>
2018-06-23 12:27:23 +0200

    Add next and prev button-handlers

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -14,4 +14,26 @@ export default class Slider extends Component{
         minSlideNumber : 0,
         currentSlideNumber : 0
     }
+    
+    nextSlideHandler = () =>{
+        let nextSlideNumber = this.state.currentSlideNumber;
+        nextSlideNumber++;
+
+        if(nextSlideNumber >= this.state.maxSlideNumber){
+            nextSlideNumber = this.state.minSlideNumber;
+        }
+        this.setState({currentSlideNumber: nextSlideNumber});
+        //console.log(nextSlideNumber);
+    }
+
+    prevSlideHandler = () =>{
+        let prevSlideNumber = this.state.currentSlideNumber;
+        prevSlideNumber--;
+
+        if(prevSlideNumber < this.state.minSlideNumber){
+            prevSlideNumber = this.state.maxSlideNumber - 1;
+        }
+        this.setState({currentSlideNumber: prevSlideNumber});
+        //console.log(prevSlideNumber);
+    }
 };
