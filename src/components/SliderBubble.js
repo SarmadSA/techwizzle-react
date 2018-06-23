42282a7db8dbcb768f12e0449d9584d86c8b237a
sarmadsa <sarmad@virusoftware.com>
2018-06-23 12:08:16 +0200

    Add slide bubble component

---
+++ src/components/SliderBubble.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/SliderBubble.css';
+
+const SliderBubble = (props) =>{
+    return(
+        <span className={props.className} onClick={props.clickHandler}></span>
+    );
+}
+
+export default SliderBubble;
