c48a238ad05fef05c075172437440140461b2e63
sarmadsa <sarmad@virusoftware.com>
2018-06-24 19:02:56 +0200

    Create profiles page

---
+++ src/pages/Profilespage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Card from '../components/Card';
+import SectionTitle from '../components/SectionTitle';
+import SearchForm from '../components/SearchForm';
+
+const Profilespage = () =>{
+    return (
+        <div>
+            <SearchForm />
+            <section>
+                <SectionTitle> Featured Product Profiles </SectionTitle>
+                <Card />
+                <Card />
+                <Card />
+            </section>
+
+            <section>
+                <SectionTitle> New Product Profiles </SectionTitle>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </section>
+        </div>
+    );
+};
+
+export default Profilespage;
