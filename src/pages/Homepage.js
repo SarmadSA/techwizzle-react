e25cfcc9d28f5bc305a6cb7f8bfdaf57aaa38ee7
sarmadsa <sarmad@virusoftware.com>
2018-06-24 19:02:43 +0200

    Add aside box

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Card from '../components/Card';
 import SectionTitle from '../components/SectionTitle';
 import Slider from '../components/Slider';
+import AsideFomBox from '../components/AsideFormBox';
 
 const Homepage = () =>{
     return (
         <div>
             <Slider />
-            
+            <AsideFomBox title="Get the latest updates"/>
+
             <section>
                 <SectionTitle> Featured Product Profiles </SectionTitle>
                 <Card />
