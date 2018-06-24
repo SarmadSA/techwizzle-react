71d75f568f66fe4247216a91f89fef091c836de0
sarmadsa <sarmad@virusoftware.com>
2018-06-24 11:10:37 +0200

    Refactor & use routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import Header from './components/Header';
-import Main from './components/Main';
+import Homepage from './components/Homepage';
 import Footer from './components/Footer';
+import {Route, BrowserRouter} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+
+          <Header />
+
+          <main className="main">
+            <Route path="/test" exact render={() => <Homepage />}/>
+
+            <br className="clear"/>
+          </main>
+
+          <Footer />
+
+        </div>
+      </BrowserRouter>
     );
   }
 }
