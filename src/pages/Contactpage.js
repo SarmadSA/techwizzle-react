fd0ed829e3ac0e60516b1b84e07c1064d49dae6c
sarmadsa <sarmad@virusoftware.com>
2018-06-24 19:02:29 +0200

    Create contact page

---
+++ src/pages/Contactpage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ContactForm from '../components/ContactForm';
+
+const Contactpage = () =>{
+    return (
+        <ContactForm />
+    );
+};
+
+export default Contactpage;
