89c7a491eb6a417167e33d7785e45c54d1e52691
sarmadsa <sarmad@virusoftware.com>
2018-06-24 18:59:05 +0200

    Rout pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import Header from './components/Header';
-import Homepage from './components/Homepage';
 import Footer from './components/Footer';
-import {Route, BrowserRouter} from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Profilespage from './pages/Profilespage';
+import Comparepage from './pages/Comparepage';
+import Aboutpage from './pages/Aboutpage';
+import Contactpage from './pages/Contactpage';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -14,8 +18,14 @@ class App extends Component {
           <Header />
 
           <main className="main">
-            <Route path="/test" exact render={() => <Homepage />}/>
-
+            <Switch>
+              <Route path="/profiles" exact component={Profilespage}/>
+              <Route path="/compare" exact component={Comparepage}/>
+              <Route path="/about" exact component={Aboutpage}/>
+              <Route path="/contact" exact component={Contactpage}/>
+              <Route path="/" component={Homepage}/>
+            </Switch>
+            
             <br className="clear"/>
           </main>
 
