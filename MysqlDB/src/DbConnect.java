767669db68794e85524e3555252a90e2b18232b7
sarmadsa <sarmad@virusoftware.com>
2018-06-29 23:42:26 +0200

    Add json importer

--- MysqlDB/src/DbConnect.java
+++ MysqlDB/src/DbConnect.java
@@ -1,11 +1,14 @@
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Iterator;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class DbConnect {
     private Connection connection;
     private Statement statement;
     private ResultSet resultSet;
+    private ArrayList<JSONObject> jsonObjcts;
     
     public DbConnect(){
         try{
@@ -13,6 +16,7 @@ public class DbConnect {
             
             this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/techwizzle", "root", "");
             this.statement = connection.createStatement();
+            this.jsonObjcts = new ArrayList<>();
         }
         catch(Exception e){
             System.out.println("Error: " + e);
@@ -34,5 +38,65 @@ public class DbConnect {
             System.out.println("Error: " + e);
         }
         return dataList.iterator();
+    }
+    
+    public void importData(){
+        try{
+            String cardsQuery = "SELECT * FROM CARDS";
+            resultSet = statement.executeQuery(cardsQuery);
+            
+            while(resultSet.next()){
+              JSONObject card = new JSONObject();
+              
+              String id = resultSet.getString("id");
+              String title = resultSet.getString("title");
+              String price = resultSet.getString("price");
+            
+              card.put("id", id);
+              card.put("title", title);
+              card.put("price", price);
+              
+              
+              
+              String gamesQuery = "SELECT * FROM GAMES WHERE ID = " + id;
+              resultSet = statement.executeQuery(gamesQuery);
+              
+              JSONArray gamesList = new JSONArray();
+              while(resultSet.next()){
+                JSONObject game = new JSONObject();
+        
+                String gameId = resultSet.getString("id");
+                String gameTitle = resultSet.getString("title");
+                String gameFPS = resultSet.getString("fps");
+
+                game.put("id", gameId);
+                game.put("title", gameTitle);
+                game.put("fps", gameFPS);
+                
+                gamesList.put(game);
+                
+              }
+              card.put("games", gamesList);
+              
+              this.jsonObjcts.add(card);
+              
+              //Debugging
+              int num = 1;
+              System.out.println(num);
+              num++;
+            }
+
+        }
+        catch(Exception e){
+            System.out.println("Error: " + e);
+        }
+    }
+    
+    public Iterator<JSONObject> getData(){
+         return this.jsonObjcts.iterator();   
+    }
+    
+    public ArrayList<JSONObject> getDataArray(){
+         return this.jsonObjcts;   
     }
 }
