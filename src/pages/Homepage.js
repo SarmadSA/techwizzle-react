16e24515680d760cbdf7c67b1bd0ed808ee6e0fb
sarmadsa <sarmad@virusoftware.com>
2018-07-02 20:17:44 +0200

    Set up dynamic data & image import

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,6 +3,13 @@ import Card from '../components/Card';
 import SectionTitle from '../components/SectionTitle';
 import Slider from '../components/Slider';
 import AsideFomBox from '../components/AsideFormBox';
+import data from '../data/cards.json';
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
 
 const Homepage = () =>{
     return (
@@ -12,22 +19,39 @@ const Homepage = () =>{
 
             <section>
                 <SectionTitle> Featured Product Profiles </SectionTitle>
-                <Card />
-                <Card />
-                <Card />
+                <Card title={data.cards[0].title}/>
+
+                {renderCards(2)}
+            
             </section>
 
             <section>
                 <SectionTitle> New Product Profiles </SectionTitle>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                
+                {renderCards(2)}
+
             </section>
         </div>
     );
 };
+
+function renderCards(number){
+    const cards = [];
+    for(let i = 0; i < number; i++){
+        const cardData = data.cards[i];
+        const images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+        cards.push(
+                    <Card 
+                        imgSrc={images[cardData.image]}
+                        title={cardData.title}
+                        dateOfRelease={cardData.dateOfRelease}
+                        price={cardData.price}
+                        productLink={cardData.link}
+                        profileLink={cardData.profile}
+                    />
+                );
+    }
+    return cards;
+}
 
 export default Homepage;
