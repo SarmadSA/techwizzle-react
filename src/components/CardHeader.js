3b51312101fc7a490cb91d2e96cfa8405e6767fa
sarmadsa <sarmad@virusoftware.com>
2018-07-02 20:13:30 +0200

    Refactor cardheader

--- src/components/CardHeader.js
+++ src/components/CardHeader.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import '../css/CardHeader.css';
 
-const CardHeader = () =>{
+const CardHeader = (props) =>{
+    const { imgSrc, title, dateOfRelease, price, productLink, profileLink } = props;
     return(
-    <div className="card-header">
-        <figure className="card-image">
-            <img src="" alt="insert the name here"/> 
-        </figure>
-        <div className="card-info-container">
-            <h3>Name of the card</h3>
-            <p>Date of release: date</p>
-            <p>AVG price: price USD</p>
-            <p>Product link</p>
-            <a href="#" className="view-profile-link">View full profile</a>
+        <div className="card-header">
+            <figure className="card-image">
+                <img src={imgSrc} alt="gtx" className="thumbnail-img"/> 
+            </figure>
+            <div className="card-info-container">
+                <h3>{title}</h3>
+                <p>Date Of Release: {dateOfRelease}</p>
+                <p>Avrage Price: {price} USD</p>
+                <a href={productLink} target="_blank" className="profile-link"> Product link</a>
+                <br/>
+                <a href={profileLink} className="profile-link">View full profile</a>
+            </div>
         </div>
-    </div>
     );
 };
 
