a1d0aedfd9fb969d274360d81d4a3557b142ac49
sarmadsa <sarmad@virusoftware.com>
2018-07-10 19:46:06 +0200

    Get data from global data provider

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,14 @@ import React from 'react';
 import CardHeader from './CardHeader';
 import GameShowcase from './GameShowcase';
 import '../css/Card.css';
+import {Link} from 'react-router-dom';
+import importAllImages from './ImageImporter';
+import DataContext, {DataProvider} from '../data/GlobalDataProvider';
 
 const Card = (props) =>{
-    const {imgSrc, title, dateOfRelease, price, productLink, profileLink} = props;
+    const numberOfGames = 3;
+    const { id, imgSrc, title, dateOfRelease, price, productLink, profileLink, gamesData } = props;
+
     return(
         <div className="card">
             <CardHeader 
@@ -16,16 +21,22 @@ const Card = (props) =>{
                 profileLink={profileLink}
             />
             <h4 className="card-content-tittle">Preformance in games:</h4>
-            <GameShowcase />
-            <GameShowcase />
-            <GameShowcase />
-            <a href={profileLink} className="more-link">
-                <div className="more-gameresults">
-                    More
-                </div>
-            </a>
+
+            {/* { renderGames(numberOfGames, gamesData) } */}
+            <DataProvider>
+                <DataContext.Consumer>
+                    {(context) => (
+                        <Link  to={"/profile/" + id} className="more-link" onClick={ () => context.setSelectedProfile(id) }>
+                            <div className="more-gameresults">
+                                More
+                            </div>
+                        </Link>
+                    )}
+                </DataContext.Consumer>
+            </DataProvider>
         </div>
     );
-};
+
+    
 
 export default Card;
