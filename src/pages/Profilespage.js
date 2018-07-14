a5b64600a351d4409f75357465bd3beebc5171c6
sarmadsa <sarmad@virusoftware.com>
2018-07-14 20:51:08 +0200

    Add dynamic load button & load more functionality

--- src/pages/Profilespage.js
+++ src/pages/Profilespage.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SectionTitle from '../components/SectionTitle';
 import SearchForm from '../components/SearchForm';
 import data from '../data/cards.json';
 import CardRenderer from '../jobs/CardRenderer';
+import LoadButton from '../components/LoadButton';
 
-const Profilespage = () =>{
-    document.title = "TechWizzle | Profiles";
-    return (
-        <div>
-            <SearchForm />
-            <section>
-                <SectionTitle> All Product Profiles </SectionTitle>
-                <CardRenderer number={3} data={data.cards}/>
-                <CardRenderer number={3} data={data.cards}/>
-                <CardRenderer number={3} data={data.cards}/>
-            </section>
-        </div>
-    );
+export default class Profilespage extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            numberOfCards: 1
+        }
+        this.loadMore = this.loadMore.bind(this);
+        this.renderLoadButton = this.renderLoadButton.bind(this);
+    }
+    
+    loadMore(){
+        if(this.state.numberOfCards <= (data.cards).length){
+            this.setState({numberOfCards: this.state.numberOfCards + 1});
+        }
+    }
+
+    renderLoadButton(){
+        if(this.state.numberOfCards <= (data.cards).length){
+            return (<LoadButton clickHandler={this.loadMore}>Load More</LoadButton>);
+        }
+    }
+
+    render(){
+        document.title = "TechWizzle | Profiles";
+        console.log(this.state.numberOfCards);
+        return (
+            <div>
+                <SearchForm />
+                <section>
+                    
+                    <SectionTitle> All Product Profiles </SectionTitle>
+                    <CardRenderer number={this.state.numberOfCards} data={data.cards}/>
+                    {this.renderLoadButton()}
+                    
+
+                </section>
+            </div>
+        );
+    }
 };
-
-export default Profilespage;
