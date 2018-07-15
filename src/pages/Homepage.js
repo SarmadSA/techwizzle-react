648eb58ec0492c4fbc6f81c31d48723c6914b584
sarmadsa <sarmad@virusoftware.com>
2018-07-15 15:03:56 +0200

    Convert to stateful class component & add click handler

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SectionTitle from '../components/SectionTitle';
 import Slider from '../components/Slider';
 import AsideFomBox from '../components/AsideFormBox';
 import data from '../data/cards.json';
 import CardRenderer from '../jobs/CardRenderer';
 import LoadButton from '../components/LoadButton';
+import History from '../jobs/History';
 
-const Homepage = () =>{
-    const numberOfFutured = 3;
-    document.title = "TechWizzle | Home";
-    return (
-        <div>
-            <Slider />
-            <AsideFomBox title="Get the latest updates"/>
+export default class Homepage extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state={
+            //If page size is enough for 3 display 3, else display 2 or 4.
+            numberOfFeatured  : 3
+        }
+        this.goToProfilesPage = this.goToProfilesPage.bind(this);
+    }
 
-            <section>
-                <SectionTitle> Featured Product Profiles </SectionTitle>
-                <CardRenderer number={3} data={data.cards}/>
-            </section>
+    goToProfilesPage(){
+        History.push('/profiles');
+    }
 
-            <section>
-                <SectionTitle> Latest Product Profiles </SectionTitle>
-                <CardRenderer number={3} data={data.cards}/>
+    render(){
+        document.title = "TechWizzle | Home";
+        
+        return (
+            <div>
+                <Slider />
+                <AsideFomBox title="Get the latest updates"/>
 
-            </section>
-            <LoadButton>Load More</LoadButton>
-        </div>
-    );
-};
+                <section>
+                    <SectionTitle> Featured Product Profiles </SectionTitle>
+                    <CardRenderer number={this.state.numberOfFeatured} data={data.cards}/>
+                </section>
 
-export default Homepage;
+                <section>
+                    <SectionTitle> Latest Product Profiles </SectionTitle>
+                    <CardRenderer number={3} data={data.cards}/>
+
+                </section>
+                
+                <LoadButton clickHandler={this.goToProfilesPage}>Load More</LoadButton>
+
+            </div>
+        );
+    }
+}
