bcc92ea4c59b40f26b2d788e878cfc98bfb7dfe2
sarmadsa <sarmad@virusoftware.com>
2018-07-31 17:24:27 +0200

    Add app initial state constants

---
+++ src/data/initStateConsts.js
@@ -0,0 +1,24 @@
+import fullData from '../data/cards.json';
+
+/**
+ * File represents the initial state of the app.
+ * Constants in this files should not be modified,
+ * changed or reassigned a new value from out side of this file.
+ */
+
+export const data = fullData.cards;
+
+export const searchFormOptions = {
+    inputValue: '',
+    isExactMatch: false,
+    searchBy: {
+        game: true,
+        settings: true,
+        resolution: true
+    },
+    fps:{
+        min: 30,
+        max: 300
+    },
+    maxPrice: 900
+};
