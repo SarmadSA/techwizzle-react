d13b6a1caee872888c6bd8818b7dabcf09d3ad82
sarmadsa <sarmad@virusoftware.com>
2018-08-14 21:53:13 +0200

    Edit initial state data

--- src/resources/initStateConsts.js
+++ src/resources/initStateConsts.js
@@ -1,12 +1,10 @@
-import fullData from './/cards.json';
-
 /**
  * File represents the initial state of the app.
  * Constants in this files should not be modified,
  * changed or reassigned a new value from out side of this file.
  */
 
-export const data = fullData.cards;
+export const data = [];
 
 export const searchFormOptions = {
     inputValue: '',
