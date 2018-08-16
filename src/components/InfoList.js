b7a1a0cd0bb8ea8a19ba0553ae75993c5204f610
sarmadsa <sarmad@virusoftware.com>
2018-08-16 20:07:53 +0200

    Add data-formatting function

--- src/components/InfoList.js
+++ src/components/InfoList.js
@@ -3,18 +3,27 @@ import '../css/InfoList.css';
 
 const InfoList = (props) =>{
     const data = props.data;
+
+    const formatData = (data) =>{
+      let dataFormat = '--';
+      if(data){
+          dataFormat = data;
+      }
+      return dataFormat;
+    };
+
     return (
         <div className="info-list-box">
             <h3>Technical information:</h3>
             <br/>
             <ul className="info-list">
-                <li> <span className="info-text-span"> CUDA Cores: </span> { data.cuda_cores } </li>
-                <li> <span className="info-text-span"> Base Clock (MHz): </span> { data.base_clock } </li>
-                <li> <span className="info-text-span"> Boost Clock (MHz): </span> { data.boost_clock } </li>
-                <li> <span className="info-text-span"> Memory Speed: </span> { data.memory_speed } </li>
-                <li> <span className="info-text-span"> Standard Memory Config: </span> { data.standard_memory_config }</li>
-                <li> <span className="info-text-span"> Memory Interface Width: </span> { data.memory_interface_width } </li>
-                <li> <span className="info-text-span"> Memory Bandwidth (GB/sec): </span> { data.memory_bandwidth } </li>
+                <li> <span className="info-text-span"> CUDA Cores: </span> { formatData(data.cuda_cores) } </li>
+                <li> <span className="info-text-span"> Base Clock (MHz): </span> { formatData(data.base_clock) } </li>
+                <li> <span className="info-text-span"> Boost Clock (MHz): </span> { formatData(data.boost_clock) } </li>
+                <li> <span className="info-text-span"> Memory Speed: </span> { formatData(data.memory_speed) } </li>
+                <li> <span className="info-text-span"> Standard Memory Config: </span> { formatData(data.standard_memory_config) } </li>
+                <li> <span className="info-text-span"> Memory Interface Width: </span> { formatData(data.memory_interface_width) } </li>
+                <li> <span className="info-text-span"> Memory Bandwidth (GB/sec): </span> { formatData(data.memory_bandwidth) } </li>
             </ul>
         </div>
     );
