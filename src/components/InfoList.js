6b0e3d9b126f252bc8f03aab8adaa2a1509993d8
sarmadsa <sarmad@virusoftware.com>
2018-08-19 15:52:59 +0200

    Edit info list

--- src/components/InfoList.js
+++ src/components/InfoList.js
@@ -20,7 +20,7 @@ const InfoList = (props) =>{
                 <li> <span className="info-text-span"> CUDA Cores: </span> { formatData(data.cuda_cores) } </li>
                 <li> <span className="info-text-span"> Base Clock (MHz): </span> { formatData(data.base_clock) } </li>
                 <li> <span className="info-text-span"> Boost Clock (MHz): </span> { formatData(data.boost_clock) } </li>
-                <li> <span className="info-text-span"> Memory Speed: </span> { formatData(data.memory_speed) } </li>
+                <li> <span className="info-text-span"> Memory Clock Speed: </span> { formatData(data.memory_clock_speed) } </li>
                 <li> <span className="info-text-span"> Standard Memory Config: </span> { formatData(data.standard_memory_config) } </li>
                 <li> <span className="info-text-span"> Memory Interface Width: </span> { formatData(data.memory_interface_width) } </li>
                 <li> <span className="info-text-span"> Memory Bandwidth (GB/sec): </span> { formatData(data.memory_bandwidth) } </li>
