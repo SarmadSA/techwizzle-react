c75e0b2137c69a0531d760ed60fbe44264941e84
Sarmad <sarmadsa2@gmail.com>
2019-02-01 21:34:37 +0100

    Set testing DB properties

--- bl/src/main/java/com/techwizzle/site/SiteApplication.java
+++ bl/src/main/java/com/techwizzle/site/SiteApplication.java
@@ -8,6 +8,7 @@ public class SiteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SiteApplication.class, args);
+        System.out.println("Working spring app");
     }
 
 }
