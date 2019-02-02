5a911a5f4fb823a8bfacb2643ba995b29d3ea691
Sarmad <sarmadsa2@gmail.com>
2019-02-02 14:56:46 +0100

    Implement get all games

--- bl/src/main/java/com/techwizzle/site/services/GameServiceImp.java
+++ bl/src/main/java/com/techwizzle/site/services/GameServiceImp.java
@@ -1,8 +1,21 @@
 package com.techwizzle.site.services;
 
+import com.techwizzle.site.model.Game;
+import com.techwizzle.site.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameServiceImp implements GameService {
+    private final GameRepository gameRepository;
 
+    @Autowired
+    public GameServiceImp(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    @Override
+    public Iterable<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
 }
