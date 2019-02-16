240964ae22ac3428afd8077099d58289251a8d51
Sarmad <sarmadsa2@gmail.com>
2019-02-16 23:06:48 +0100

    Handle changes, clicks and submitting outside of the component

--- ui/src/components/ContactForm.js
+++ ui/src/components/ContactForm.js
@@ -1,39 +1,63 @@
 import React from 'react';
 import '../css/ContactForm.css';
 
-const ContactForm = () =>{
+const ContactForm = (props) =>{
+
     return (
         <div>
             <h1 className="page-tittle">Contact us</h1>
             <br/>
+            <form className="contact-form" method="post">
+                { props.children }
 
-            <form className="contact-form" action="" method="post">
-
-                <label for="name">Name:</label>
+                <label htmlFor="name">Name:</label>
                 <br/>
-                <input type="text" name="name" id="name" className="form-input input-placeholder focus-style" placeholder="Your name.."/>
+                <input type="text"
+                       name="name"
+                       id="name"
+                       className="form-input input-placeholder focus-style"
+                       placeholder="Your name.."
+                       onChange={ props.handleNameChange } />
                 <span className="error-message">  </span>
                 <br/>
 
-                <label for="email">E-mail:</label>
+                <label htmlFor="email">E-mail:</label>
                 <br/>
-                <input type="text" name="email" id="email" className="form-input input-placeholder focus-style" placeholder="Your email.."/>
+                <input type="text"
+                       name="email"
+                       id="email"
+                       className="form-input input-placeholder focus-style"
+                       placeholder="Your email.."
+                       onChange={ props.handleEmailChange } />
                 <span className="error-message"> * </span>
                 <br/>
 
-                <label for="subject">Subject:</label>
+                <label htmlFor="subject">Subject:</label>
                 <br/>
-                <input type="text" name="subject" id="subject" className="form-input input-placeholder focus-style" placeholder="Enter the subject.."/>
+                <input type="text"
+                       name="subject"
+                       id="subject"
+                       className="form-input input-placeholder focus-style"
+                       placeholder="Enter the subject.."
+                       onChange={ props.handleSubjectChange }/>
                 <span className="error-message"> * </span>
                 <br/>
 
-                <label for="message">Message:</label>
+                <label htmlFor="message">Message:</label>
                 <span className="error-message"> * </span>
                 <br/>
-                <textarea id="message" name="message" className="contact-message input-placeholder focus-style" placeholder="Write your message.."/>
+                <textarea id="message"
+                          name="message"
+                          className="contact-message input-placeholder focus-style"
+                          placeholder="Write your message.."
+                          onChange={ props.handleMessageChange }/>
                 <br/>
 
-                <input type="submit" name="submit" className="submit-button focus-style" value="Send"/>
+                <input type="submit"
+                       name="submit"
+                       className="submit-button focus-style"
+                       value="Send"
+                       onClick={ props.handleClick }/>
             </form>
         </div>
     );
