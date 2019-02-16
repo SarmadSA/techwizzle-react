3218671c69b0b01d5faa689d0a7c9ad1cc5c84fc
Sarmad <sarmadsa2@gmail.com>
2019-02-16 16:04:02 +0100

    Create contact form message repository

---
+++ bl/src/main/java/com/techwizzle/site/repositories/ContactFormMessageRepository.java
@@ -0,0 +1,8 @@
+package com.techwizzle.site.repositories;
+
+import com.techwizzle.site.model.ContactFormMessage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContactFormMessageRepository extends CrudRepository<ContactFormMessage, Long> {
+
+}
