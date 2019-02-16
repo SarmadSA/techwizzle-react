f223f3af5d2101f1a60fd7319fb8803ace510f53
Sarmad <sarmadsa2@gmail.com>
2019-02-16 16:05:23 +0100

    Create contact form message controller with submitting rest API

---
+++ bl/src/main/java/com/techwizzle/site/controllers/ContactFormMessageController.java
@@ -0,0 +1,30 @@
+package com.techwizzle.site.controllers;
+
+import com.techwizzle.site.services.ContactFormMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*") //allow only this domain to perform cross requests.
+@RequestMapping(path="/contact")
+public class ContactFormMessageController {
+
+    private final ContactFormMessageService contactFormMessageService;
+
+    @Autowired
+    public ContactFormMessageController(ContactFormMessageService contactFormMessageService) {
+        this.contactFormMessageService = contactFormMessageService;
+    }
+
+    @PostMapping(path="/submit")
+    public String submitMessage(@RequestParam String name, @RequestParam String email, @RequestParam String subject, @RequestParam String message){
+        String response = "Something went wrong!"; //TODO - make a better response
+
+        boolean successfullyCreated = contactFormMessageService.createContactFormMessage(name, email, subject, message);
+        if(successfullyCreated){
+            response = "Message submitted successfully. Thank you for reaching out!";
+        }
+
+        return response;
+    }
+}
