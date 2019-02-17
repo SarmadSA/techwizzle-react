5ade65d718f2f9284cfe4dc6b34e5e5ce9634948
Sarmad <sarmadsa2@gmail.com>
2019-02-17 03:25:17 +0100

    Implemented defensive card-renderer

--- ui/src/utils/CardRenderer.js
+++ ui/src/utils/CardRenderer.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import Card from '../components/Card';
 import ImageImporter from './ImageImporter';
+import {valueOrDefault} from "../helpers/helperFunctions";
+import {PROFILE} from '../resources/defaultData';
 
 const CardRenderer = (props) =>{
     const images = ImageImporter(require.context('../images', false, /\.(png|jpe?g|svg)$/));
     const cards = [];
+
+    const getImage = (data) => {
+        if(null != data.externalImageLink){
+            return data.externalImageLink;
+        } else if(null != data.internalImageLink){
+            return images[data.internalImageLink];
+        } else{
+            return images[PROFILE.IMAGE_LINK];
+        }
+    };
+
     if(props.data){
         for(let i = 0; i < props.number; i++){
             const cardData = props.data[i];
             if(cardData){
                 cards.push(
                     <Card
-                        key={cardData.id}
-                        id={cardData.id}
-                        imgSrc={cardData.externalImageLink ? cardData.externalImageLink : images[cardData.internalImageLink]}
-                        title={cardData.shortTitle}
-                        dateOfRelease={cardData.release_date}
-                        price={cardData.averagePrice}
-                        productLink={cardData.amazonLink}
-                        profileLink={"/profile/" + cardData.id}
-                        gamesData={cardData.benchmarks}
+                        key={ i }
+                        id={ cardData.id }
+                        imgSrc={ getImage(cardData) }
+                        title={ valueOrDefault(cardData.shortTitle, PROFILE.TITLE) }
+                        dateOfRelease={ valueOrDefault(cardData.releaseDate, PROFILE.RELEASE_DATE) }
+                        price={ valueOrDefault(cardData.averagePrice, PROFILE.AVERAGE_PRICE) }
+                        productLink={ valueOrDefault(cardData.amazonLink, PROFILE.AMAZON_LINK) }
+                        profileLink={ "/profile/" + cardData.id }
+                        benchmarks={ cardData.benchmarks }
                     />
                 );
             }
