21ae209ce0bf33b0aa07730676eaacb4deae889e
Sarmad <sarmadsa2@gmail.com>
2019-02-17 13:09:24 +0100

    Externalize default values

---
+++ ui/src/resources/defaultData.js
@@ -0,0 +1,30 @@
+export const PROFILE = {
+    TITLE: '--',
+    RELEASE_DATE: '--',
+    AVERAGE_PRICE: '--',
+    PROFILE_LINK: '',
+    AMAZON_LINK: '', //TODO: put amazon link with ref to amazon (not a specific product)
+    IMAGE_LINK: '', //TODO: put default image link (if product have no image)
+    TECHNICAL_INFO: {
+        CUDA_CORES: '--',
+        BASE_CLOCK: '--',
+        BOOST_CLOCK: '--',
+        MEMORY_CLOCK_SPEED: '--',
+        STANDARD_MEMORY_CONFIG: '--',
+        MEMORY_INTERFACE_WIDTH: '--',
+        MEMORY_BANDWIDTH: '--'
+    }
+};
+
+export const GAME = {
+    TITLE: '--',
+    IMAGE_LINK: '', //TODO: put default image link (if game have no image) (maybe use same default as profile?)
+    RELEASE_DATE: '--'
+};
+
+export const BENCHMARK = {
+    AVERAGE_FPS: '??',
+    SETTINGS: '--',
+    RESOLUTION: '--',
+    TEST_DATE: '--'
+};
