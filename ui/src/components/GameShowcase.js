8ddc79b2ca494a8f1c429caf38cbd275ce50a797
Sarmad <sarmadsa2@gmail.com>
2019-02-17 13:12:37 +0100

    Add hardcoded resolution unit

--- ui/src/components/GameShowcase.js
+++ ui/src/components/GameShowcase.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hexagon from './Hexagon';
 import '../css/GameShowcase.css';
 
+//TODO: Get resolution unit from database (p, k, 1080p, 4k etc...)
 const GameShowcase = (props) =>{
     const { imgSrc, title, settings, fps, resolution } = props;
     return(
@@ -12,7 +13,7 @@ const GameShowcase = (props) =>{
 
             <div className="game-results-container">
                 <h3 className="game-result">{title}</h3>
-                <p className="game-result">{settings} settings - {resolution}</p>
+                <p className="game-result">{settings} settings - {resolution}p</p>
             </div>
             
             <Hexagon fps={fps}/>
