eacf8d0b5b9770b2760eb8fddddc74e74f87dc5c
Sarmad <sarmadsa2@gmail.com>
2019-02-17 12:54:12 +0100

    Check for null value before setting the attributes

--- ui/src/utils/GameRenderer.js
+++ ui/src/utils/GameRenderer.js
@@ -2,10 +2,25 @@ import React from 'react';
 import GameShowcase from '../components/GameShowcase';
 import ImageImporter from './ImageImporter';
 import FlexErrorBox from "../components/FlexErrorBox";
+import {valueOrDefault} from "../helpers/helperFunctions";
+import {GAME, BENCHMARK} from '../resources/defaultData';
+import {CARD_FEEDBACK} from '../resources/feedbackMessages';
 
 const GameRenderer = (props) =>{
     const gamesArray = [];
     const images = ImageImporter(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+
+    //TODO: Replace with common general helper function in all renderers
+    const getImage = (data) => {
+        if(null != data.externalImageLink){
+            return data.externalImageLink;
+        } else if(null != data.internalImageLink){
+            return images[data.internalImageLink];
+        } else{
+            return images[GAME.IMAGE_LINK];
+        }
+    };
+
     if(props.data && props.data.length > 0){
         if(props.data.length > props.number){
             renderGames(props.number);
@@ -15,19 +30,19 @@ const GameRenderer = (props) =>{
         }
     }
     else{
-        gamesArray.push (<FlexErrorBox>No game tests available!</FlexErrorBox>);
+        gamesArray.push (<FlexErrorBox> { CARD_FEEDBACK.NO_GAME_TESTS } </FlexErrorBox>);
     }
 
     function renderGames(number){
         for(let i = 0; i < number; i++){
             gamesArray.push (
                 <GameShowcase
-                    // key={props.data[i].uid}
-                    imgSrc = {images[props.data[i].image]}
-                    title = {props.data[i].title}
-                    settings = {props.data[i].settings}
-                    resolution = {props.data[i].resolution}
-                    fps = {props.data[i].fps}
+                    key={i}
+                    imgSrc = { getImage(props.data[i].game) }
+                    title = { valueOrDefault(props.data[i].game.title, GAME.TITLE) }
+                    settings = { valueOrDefault(props.data[i].settings, BENCHMARK.SETTINGS) }
+                    resolution = { valueOrDefault(props.data[i].resolution, BENCHMARK.RESOLUTION) }
+                    fps = { valueOrDefault(props.data[i].avgFps, BENCHMARK.AVERAGE_FPS) }
                 />
             );
         }
