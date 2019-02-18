2f4c098afc998f9d5864db9eb6c357f986579f58
Sarmad <sarmadsa2@gmail.com>
2019-02-18 15:02:06 +0100

    Rest refactor commit

--- UI/techwizzle-react/src/resources/pageTitles.js
+++
@@ -1,8 +0,0 @@
-export const MAIN = 'TechWizzle';
-export const VB = ' | ';
-export const HOME = MAIN + VB + 'Home';
-export const NOT_FOUND = MAIN + VB + 'Page not found';
-export const CONTACT = MAIN + VB + 'Contact';
-export const ABOUT = MAIN + VB + 'About';
-export const PROFILES = MAIN + VB + 'Profiles';
-export const COMPARE = MAIN + VB + 'Compare';
