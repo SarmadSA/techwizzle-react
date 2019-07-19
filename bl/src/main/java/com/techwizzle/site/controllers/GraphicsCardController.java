a4212975e621178aec2dbdfd18bb05b82d5f6a0a
sarmadsa <sarmad@virusoftware.com>
2019-07-20 01:59:33 +0200

    Implement add GPU endpoint

--- bl/src/main/java/com/techwizzle/site/controllers/GraphicsCardController.java
+++ bl/src/main/java/com/techwizzle/site/controllers/GraphicsCardController.java
@@ -2,27 +2,38 @@ package com.techwizzle.site.controllers;
 
 import com.techwizzle.site.model.GraphicsCard;
 import com.techwizzle.site.services.GraphicsCardService;
+import com.techwizzle.site.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
 
 @RestController
 @RequestMapping("/graphicsCard")
 @CrossOrigin //TODO - allow only authorized domains
 public class GraphicsCardController {
 
-    private final GraphicsCardService cardService;
+    private final GraphicsCardService graphicsCardService;
 
     @Autowired
     public GraphicsCardController(GraphicsCardService cardService) {
-        this.cardService = cardService;
+        this.graphicsCardService = cardService;
     }
 
     @GetMapping("/list")
-    public Iterable<GraphicsCard> listCards(){
-        return cardService.getAllGraphicsCards();
+    public Iterable<GraphicsCard> listGraphicsCards(){
+        return graphicsCardService.getAllGraphicsCards();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity addGraphicsCard(@Valid @RequestBody GraphicsCard graphicsCard){
+        if(this.graphicsCardService.addGraphicsCard(graphicsCard)){
+            return new ResponseEntity<>(Constants.ADD_SUCCESS , HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(Constants.ADD_FAILURE, HttpStatus.BAD_REQUEST);
     }
 
 }
---
+++ bl/src/main/java/com/techwizzle/site/util/Constants.java
@@ -0,0 +1,6 @@
+package com.techwizzle.site.util;
+
+public class Constants {
+    public static final String ADD_SUCCESS = "Successfully added!";
+    public static final String ADD_FAILURE = "Failed to add!";
+}
