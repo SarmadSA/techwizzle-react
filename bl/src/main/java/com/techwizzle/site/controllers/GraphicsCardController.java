68b2b396fd1e1a394759dce01b652158f04391dc
sarmadsa <sarmad@virusoftware.com>
2020-03-21 23:13:18 +0100

    Refactor & add new request mappings

--- bl/src/main/java/com/techwizzle/site/controllers/GraphicsCardController.java
+++ bl/src/main/java/com/techwizzle/site/controllers/GraphicsCardController.java
@@ -1,18 +1,22 @@
 package com.techwizzle.site.controllers;
 
+import com.techwizzle.site.exception.GraphicsCardServiceException;
 import com.techwizzle.site.model.GraphicsCard;
+import com.techwizzle.site.response.ErrorMessage;
 import com.techwizzle.site.services.GraphicsCardService;
 import com.techwizzle.site.util.Constants;
+import com.techwizzle.site.util.Helper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/graphicsCard")
+@RequestMapping("/graphics-card")
 @CrossOrigin //TODO - allow only authorized domains
 public class GraphicsCardController {
 
@@ -23,9 +27,39 @@ public class GraphicsCardController {
         this.graphicsCardService = cardService;
     }
 
-    @GetMapping("/list")
-    public Iterable<GraphicsCard> listGraphicsCards(){
-        return graphicsCardService.getAllGraphicsCards();
+    /***
+     *
+     * @param page
+     * @param size -
+     * @param sortBy - field to sort results by
+     * @return
+     */
+    @GetMapping("/list") //TODO default sort should be by latest release
+    public List<GraphicsCard> listGraphicsCards(@RequestParam(value = "page", defaultValue = "0", required = false) int page,
+                                                @RequestParam(value = "size", defaultValue = "6", required = false) int size,
+                                                @RequestParam(value = "sort", defaultValue = "timestamp:ASC", required = false) String[] sortBy){
+        return graphicsCardService.getAllGraphicsCards(Helper.getPagable(page, size, sortBy));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getGraphicsCardById(@PathVariable("id") String id){
+        try{
+            return new ResponseEntity<>(graphicsCardService.findByID(id), HttpStatus.OK);
+        } catch (GraphicsCardServiceException e){
+            ErrorMessage errorMessage = new ErrorMessage()
+                    .setMessage(e.getMessage())
+                    .setPath("/graphics-card/" + id)
+                    .addErrors(e.getErrors());
+            return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/search")
+    public List<GraphicsCard> searchGraphicsCardsByTitle(@RequestParam String title,
+                                                         @RequestParam(value = "page", defaultValue = "0", required = false) int page,
+                                                         @RequestParam(value = "size", defaultValue = "6", required = false) int size,
+                                                         @RequestParam(value = "sort", defaultValue = "timestamp:ASC", required = false) String[] sortBy){
+        return graphicsCardService.searchByTitle(title, Helper.getPagable(page, size, sortBy));
     }
 
     @PostMapping("/add")
@@ -34,6 +68,14 @@ public class GraphicsCardController {
             return new ResponseEntity<>(Constants.ADD_SUCCESS , HttpStatus.CREATED);
         }
         return new ResponseEntity<>(Constants.ADD_FAILURE, HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/remove")
+    public ResponseEntity removeGraphicsCard(@RequestParam String id){
+        //if(this.graphicsCardService.removeGraphicsCard(Integer.parseInt(id))){
+        //    return new ResponseEntity<>(Constants.DELETE_SUCCESS, HttpStatus.OK);
+        //}
+        return new ResponseEntity<>(Constants.DELETE_FAILURE, HttpStatus.BAD_REQUEST);
     }
 
 }
