d8545673bdf136b8feb185aba1b61199ac49e4e2
sarmadsa <sarmad@virusoftware.com>
2020-03-22 00:14:27 +0100

    Return custom message to client for any exception

--- bl/src/main/java/com/techwizzle/site/exception/ApplicationExceptionHandler.java
+++ bl/src/main/java/com/techwizzle/site/exception/ApplicationExceptionHandler.java
@@ -15,16 +15,21 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleAnyException(Exception e, WebRequest request){
-        return new ResponseEntity<>(e, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @ExceptionHandler(value = {GraphicsCardServiceException.class})
-    public ResponseEntity<Object> handelGraphicsCardServiceException(GraphicsCardServiceException e, WebRequest request){
+        e.printStackTrace();
         ErrorMessage errorMessage = new ErrorMessage()
-                .setMessage(e.getMessage())
-                .setPath(request.getContextPath());
-                //.addError(e.getCause().getMessage());
+                .setMessage("Something went wrong")
+                .setPath(request.getContextPath())
+                .addSingleError("Something went wrong while processing the request!");
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    //@ExceptionHandler(value = {GraphicsCardServiceException.class})
+    //public ResponseEntity<Object> handelGraphicsCardServiceException(GraphicsCardServiceException e, WebRequest request){
+    //    ErrorMessage errorMessage = new ErrorMessage()
+    //            .setMessage(e.getMessage())
+    //            .setPath(request.getContextPath());
+    //            //.addError(e.getCause().getMessage());
+    //    return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    //}
 
 }
