c2411d620ff5311229ab591954103bbe22fefb5c
sarmadsa <sarmad@virusoftware.com>
2020-03-21 23:12:41 +0100

    Refactor GPU repo

--- bl/src/main/java/com/techwizzle/site/repositories/CardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.techwizzle.site.repositories;
-
-import com.techwizzle.site.model.GraphicsCard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<GraphicsCard, Integer> {
-
-}
---
+++ bl/src/main/java/com/techwizzle/site/repositories/GraphicsCardRepository.java
@@ -0,0 +1,22 @@
+package com.techwizzle.site.repositories;
+
+import com.techwizzle.site.model.GraphicsCard;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+import java.util.List;
+
+public interface GraphicsCardRepository extends JpaRepository<GraphicsCard, Integer> {
+    //@Query("SELECT *")
+    //List<GraphicsCard> findAllWithTitle(String title, Pageable pageable);
+
+    List<GraphicsCard> findAllByMemorySize(Integer memorySize, Pageable pageable);
+
+    GraphicsCard getByUuid(String uuid);
+
+    @Query("SELECT gpu FROM GraphicsCard gpu WHERE gpu.title LIKE %?1%")
+    List<GraphicsCard> searchByTitle(String title, Pageable pageable);
+
+}
